b55dbc1511f2f33c55ab35739ea9abf9cc435eb4
Erin Stevenson <estevenson_94@live.com>
2019-02-12 14:49:35 +0000

    rewrote test

--- server/spec/models/spacesSpec.js
+++ server/spec/models/spacesSpec.js
@@ -1,11 +1,10 @@
 
-const spacesController = require('../../controllers').spaces;
+const spaces = require('../../controllers').spaces;
 
 describe('Spaces', function() {
   it('returns a list of spaces', function() {
   
-    module.exports = { 
-      expect(spacesController.list).toEqual(1)
-    };
-  }
-}
+      expect(spaces.list).toEqual(1)
+    
+  });
+});
