35fc65f6d384d1b66cda202f3a47a67960bb3ca8
AdrienFabre <adrien.fabre.1@gmail.com>
2019-02-12 16:25:21 +0000

    Form styling completed

--- client/src/components/CreateSpace/index.js
+++ client/src/components/CreateSpace/index.js
@@ -52,13 +52,33 @@ const StyledForm = styled(Form)`
     width: 100%;
     font-size: 16px;
     margin-bottom: 20px;
-    border: 1px solid #f4f5f7;
+    border: 2px solid #cccacb;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
   }
+
+  input:focus,
+  textarea:focus {
+    border: 2px solid palevioletred;
+    outline: none;
+  }
+
 
   input[type='submit'] {
     background-color: palevioletred;
+    border: 2px solid palevioletred;
+    color: white;
+    border-radius: 7px;
+    width: 100%;
+  }
+
+  input[type='submit']:hover {
+    background-color: #b74b6e;
+    border: 2px solid #b74b6e;
     color: white;
     width: 100%;
+    border-radius: 7px;
+    cursor: pointer;
+  
   }
 `
 
