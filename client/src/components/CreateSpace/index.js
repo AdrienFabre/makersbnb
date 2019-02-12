04e3f4a28e9d84912a78ba904437a6efd352a85d
Ibrahim Butt <ibrahimbutt@me.com>
2019-02-12 11:00:11 +0000

    WIP Page Title Component

--- client/src/components/CreateSpace/index.js
+++ client/src/components/CreateSpace/index.js
@@ -1,12 +1,20 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const PageTitle = props => (
+  <div className={props.className}>
+    <h2>{props.title}</h2>
+    <p>{props.description}</p>
+  </div>
+)
+
+const StyledPageTitle = styled(PageTitle)``
 
 const CreateSpace = () => (
   <React.Fragment>
-    <h2>Book a Space</h2>
-    <a href="/spaces/create">List a Space</a>
-    <h3>Beautiful Relaxing Space</h3>
+    <StyledPageTitle title="List a Space" description="A description" />
   </React.Fragment>
 )
 
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -5,13 +5,18 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import Header from './Header'
 import Spaces from './Spaces'
+import CreateSpace from './CreateSpace'
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Header />
-      <Route path="/spaces" render={props => <Spaces />} />
-      <Route path="/spaces/create" render={props => <CreateSpace />} />
+      <Route exact={true} path="/spaces" render={props => <Spaces />} />
+      <Route
+        exact={true}
+        path="/spaces/create"
+        render={props => <CreateSpace />}
+      />
     </div>
   </BrowserRouter>
 )
