65a9fccacb612451445e8703ca217267b17af06f
William Dunk <williamdunk@gmail.com>
2019-02-12 10:56:53 +0000

    styled spaces page Hero component

--- client/src/components/Spaces/index.js
+++ client/src/components/Spaces/index.js
@@ -2,10 +2,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import styled from "styled-components";
+
+const Hero = props => (
+  <div className={props.className}>
+    <h2>Book a Space</h2>
+  </div>
+);
+
+const StyledHero = styled(Hero)`
+  background-image: url("https://images.unsplash.com/photo-1533667586627-9f5ddbd42539?ixlib");
+  background-color: #cccccc;
+  height: 500px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    font-size: 8em;
+    color: white;
+  }
+`;
+
+const Listing = props => (
+  <div className={props.className}>
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </div>
+);
+
 const Spaces = () => (
   <React.Fragment>
-    <h2>Book a Space</h2>
-    <h3>Beautiful Relaxing Space</h3>
+    <StyledHero />
+    <Listing title="A Beautiful Relaxing Place" description="A description" />
   </React.Fragment>
 );
 
