703657a42db83d77122bdf0ab503d89cbf6310e9
Erin Stevenson <estevenson_94@live.com>
2019-02-13 15:29:01 +0000

    Added create and list controllers and space creation api
    Co-authored-by: ibrahimbutt1993@gmail.com

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-const Space = require('../models/space')
+const Space = require('../controllers/spaces')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/api/spaces', (req, res) => {
-  Space.findAll().then(space => {
-    res.send(space)
-  })
+  Space.list(res)
+})
+
+app.post('/api/spaces/create', (req, res) => {
+  Space.create(req, res)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
